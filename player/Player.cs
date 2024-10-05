6abcd0fd46a5cd581f245d151d31ac42dcd772dc
Ilari Tommiska <ilari97@hotmail.com>
2024-10-05 13:07:17 +0300

    Anim magic

--- player/Player.cs
+++ player/Player.cs
@@ -8,6 +8,9 @@ public partial class Player : CharacterBody2D {
 	[Export]
 	public float Friction = 10.0f;
 
+	[Export]
+	public AnimationPlayer? Animation;
+
 	public override void _Ready() {
 		base._Ready();
 		var spawns = GetTree().GetNodesInGroup("PlayerSpawn");
@@ -33,16 +36,28 @@ public partial class Player : CharacterBody2D {
 		var delta = (float)_delta;
 
 		var direction = Input.GetVector("left", "right", "up", "down");
+		var animationDirection = direction.X < 0.0
+			? "left"
+			: direction.X > 0.0
+			? "right"
+			: direction.Y < 0.0
+			? "left"
+			: "right";
 		if (direction.LengthSquared() > 0.001f) {
 			Velocity = direction * Speed * delta;
+
+			Animation?.Play($"walk_{animationDirection}");
 		} else {
 			var currentSpeed = Velocity.Length();
 			Velocity = new(
-				Mathf.MoveToward(Velocity.X, 0, (currentSpeed * Friction) * delta),
-				Mathf.MoveToward(Velocity.Y, 0, (currentSpeed * Friction) * delta)
+				Mathf.MoveToward(Velocity.X, 0, currentSpeed * Friction * delta),
+				Mathf.MoveToward(Velocity.Y, 0, currentSpeed * Friction * delta)
 			);
 		}
 
+		if (Velocity.LengthSquared() < 0.01f) {
+			Animation?.Play($"idle_{animationDirection}");
+		}
 		MoveAndCollide(Velocity);
 	}
 }
