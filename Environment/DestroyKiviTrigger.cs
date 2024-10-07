84b913c951e95e60c77a8034f77fb7cc34955220
Ilari Tommiska <ilari97@hotmail.com>
2024-10-07 22:01:28 +0300

    dialogue on boom

--- Environment/DestroyKiviTrigger.cs
+++ Environment/DestroyKiviTrigger.cs
@@ -5,9 +5,16 @@ public partial class DestroyKiviTrigger : Area2D {
 	[Export]
 	public Node2D? objectToDestroy;
 
+	[Export]
+	public DialogueTree? DialogueTree;
+
 	public void bodyEntered(Node2D node) {
 		if (node is Player) {
 			objectToDestroy?.QueueFree();
+
+			if (DialogueTree is not null) {
+				Dialogue.Instance(this).StartDialogue(DialogueTree);
+			}
 		}
 	}
 }
