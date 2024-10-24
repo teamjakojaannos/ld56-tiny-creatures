77d13c1dc17f671da6dc15f48c2312feafb6474c
Ilari Tommiska <ilari97@hotmail.com>
2024-10-24 17:31:49 +0300

    support entrance offsets for level transitions
    This allows creating the levels positioned at the origin and requires
    worrying about the relative offsets only once transitions are
    configured. In practice, this means that levels can be created
    indenpendently without having to position the scenes at awkward
    coordinates.
    
    Got an excuse to do some funky scene file queries (reading nodes from a
    scene without instantiating it in-editor), which turned out quite fun.
    There is now a programmatically created enum (dropdown) property for
    level transitions, which lists all nodes in 'LevelEntrance' group as
    options. Then, there are a bunch of other magic properties for handling
    the backing storage to the scene file and whatnot.
    
    While at it, converted the scene preview to a programmatically created
    property, too, as it is not really a value stored inside the node, but
    rather a debug toggle.
    
    There is quite a bit of magical boilerplate related to all this. I hope
    to get around to clean up things a bit at some point.

---
+++ Groups.cs
@@ -0,0 +1,5 @@
+public static class Groups {
+    public static class Name {
+        public const string LEVEL_ENTRANCE = "LevelEntrance";
+    }
+}
--- levels/LevelTransition.cs
+++ levels/LevelTransition.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Godot;
 
@@ -11,38 +12,31 @@ public partial class LevelTransition : Area2D {
     [Export(PropertyHint.File, "*.tscn")]
     [MustSetInEditor]
     public string OtherScene {
-        get => _otherScene ?? this.AssertNotNullOutsideEditor<string>();
-        set {
-            _otherScene = value;
-            UpdateConfigurationWarnings();
-        }
+        get => this.GetNotNullExportPropertyWithNullableBackingField(_otherScene);
+        set => this.SetExportProperty(ref _otherScene, value, notifyPropertyListChanged: true);
     }
     private string? _otherScene;
 
     [Export]
-    public bool PreviewOtherScene {
-        get => _previewOtherScene && _previewScene is not null && _previewScene.IsInsideTree() && !_previewScene.IsQueuedForDeletion();
-        set {
-            if (Engine.IsEditorHint()) {
-                _previewOtherScene = value;
+    private NodePath? entranceNodePath;
 
-                if (_previewScene is not null) {
-                    _previewScene.GetParent()?.RemoveChild(_previewScene);
-                    _previewScene.QueueFree();
-                    _previewScene = null;
-                }
-
-                if (_previewOtherScene && _otherScene is not null) {
-                    var resource = ResourceLoader.Load<PackedScene>(_otherScene);
-                    _previewScene = resource.Instantiate<Level>();
-                    _previewScene.Name = "Other scene preview";
-                    AddChild(_previewScene);
-                }
-            }
-        }
+    public bool IsOtherScenePreviewVisible {
+        get => _previewScene is not null && _previewScene.IsInsideTree() && !_previewScene.IsQueuedForDeletion();
     }
 
-    private bool _previewOtherScene = false;
+    private void UpdatePreviewPosition() {
+        if (_previewScene is null) {
+            return;
+        }
+
+        var anchorNode = entranceNodePath is not null
+                                ? _previewScene.GetNodeOrNull<Node2D>(entranceNodePath)
+                                : null;
+        var anchorOffset = anchorNode?.Position ?? Vector2.Zero;
+
+        _previewScene.GlobalPosition = GlobalPosition - anchorOffset;
+    }
+
     private Level? _previewScene;
 
     public override string[] _GetConfigurationWarnings() {
@@ -51,7 +45,152 @@ public partial class LevelTransition : Area2D {
             .ToArray();
     }
 
+	public override void _ValidateProperty(Godot.Collections.Dictionary property) {
+        base._ValidateProperty(property);
+
+         if (property["name"].AsStringName() == PropertyName.entranceNodePath) {
+            var usage = PropertyUsageFlags.Storage | PropertyUsageFlags.NoEditor;
+            property["usage"] = (int)usage;
+        }
+	}
+
+	public override Godot.Collections.Array<Godot.Collections.Dictionary> _GetPropertyList() {
+        var properties = base._GetPropertyList() ?? new Godot.Collections.Array<Godot.Collections.Dictionary>();
+
+        // If the other scene is specified, obtain a list of valid entrance
+        // nodes and show a selection dropdown.
+        if (_otherScene is not null) {
+            var entranceNodeNames = FindOtherSceneEntrances()
+                .Select(node => node.Item2);
+
+            var hintString =
+                !entranceNodeNames.Any()
+                    ? "<none available>"
+                    : string.Join(",", entranceNodeNames);
+            properties.Add(new Godot.Collections.Dictionary() {
+                { "name", "EntranceNode" },
+                { "type", (int)Variant.Type.Int },
+                { "hint", (int)PropertyHint.Enum },
+                { "hint_string", hintString },
+                { "usage", (int)(PropertyUsageFlags.Editor | PropertyUsageFlags.NoInstanceState) },
+            });
+
+            properties.Add(new Godot.Collections.Dictionary() {
+                { "name", "OtherScenePreview" },
+                { "type", (int)Variant.Type.Bool },
+                { "usage", (int)(PropertyUsageFlags.Editor | PropertyUsageFlags.NoInstanceState) },
+            });
+        }
+
+        return properties;
+    }
+
+    public override Variant _Get(StringName property) {
+        string propertyName = property.ToString();
+        if (propertyName == "EntranceNode") {
+            if (entranceNodePath is not null) {
+                var entranceNodes = FindOtherSceneEntrances();
+
+                for (int optionIndex = 0; optionIndex < entranceNodes.Count; optionIndex++) {
+                    var (path, _) = entranceNodes[optionIndex];
+                    if (path == entranceNodePath) {
+                        return optionIndex;
+                    }
+                }
+            }
+
+            entranceNodePath = null;
+            NotifyPropertyListChanged();
+            return 0;
+        } else if (propertyName == "OtherScenePreview") {
+            return IsOtherScenePreviewVisible;
+        }
+
+        return default;
+    }
+
+    public override bool _Set(StringName property, Variant value) {
+        string propertyName = property.ToString();
+        if (propertyName == "EntranceNode") {
+            var optionIndex = value.AsInt32();
+            TrySelectEntranceNode(optionIndex);
+            UpdatePreviewPosition();
+
+            return true;
+        } else if (propertyName == "OtherScenePreview" && Engine.IsEditorHint()) {
+            var valueBool = value.AsBool();
+            if (valueBool && _otherScene is null) {
+                GD.PrintErr("Cannot preview a scene: other scene is not configured!");
+                return true;
+            }
+
+            if (_previewScene is not null) {
+                _previewScene.GetParent()?.RemoveChild(_previewScene);
+                _previewScene.QueueFree();
+                _previewScene = null;
+            }
+
+            if (valueBool) {
+                var resource = ResourceLoader.Load<PackedScene>(_otherScene);
+                _previewScene = resource.Instantiate<Level>();
+                _previewScene.Name = "Other scene preview";
+
+                if (entranceNodePath is null) {
+                    TrySelectEntranceNode(0);
+                }
+
+                UpdatePreviewPosition();
+                AddChild(_previewScene);
+            }
+        }
+        return false;
+    }
+
+    private void TrySelectEntranceNode(int optionIndex) {
+        var entranceNodes = FindOtherSceneEntrances();
+        if (!entranceNodes.Any()) {
+            entranceNodePath = null;
+            UpdatePreviewPosition();
+            return;
+        }
+
+        var (path, _) = entranceNodes[Mathf.Min(optionIndex, entranceNodes.Count - 1)];
+        entranceNodePath = path;
+    }
+
+    private List<(NodePath, StringName)> FindOtherSceneEntrances() {
+        // Other scene is not properly set yet => return empty list
+        if (_otherScene is null || _otherScene.Trim() == "" || _otherScene.Trim() == "res://") {
+            return new();
+        }
+
+        var entranceNodes = new List<(NodePath, StringName)>();
+
+        var resource = ResourceLoader.Load<PackedScene>(OtherScene);
+        var sceneState = resource.GetState();
+        var nodesInScene = sceneState.GetNodeCount();
+        for (int nodeIndex = 0; nodeIndex < nodesInScene; nodeIndex++) {
+            var isEntranceNode = sceneState.GetNodeGroups(nodeIndex).Contains(Groups.Name.LEVEL_ENTRANCE);
+            if (!isEntranceNode) {
+                continue;
+            }
+
+            entranceNodes.Add((
+                sceneState.GetNodePath(nodeIndex),
+                sceneState.GetNodeName(nodeIndex)
+            ));
+        }
+
+        return entranceNodes;
+    }
+
     public Vector2 ExitDirection => Transform.BasisXform(Vector2.Right);
+
+    public override void _ExitTree() {
+        base._ExitTree();
+        _previewScene?.QueueFree();
+        _previewScene = null;
+    }
 
     public override void _Ready() {
         base._Ready();
@@ -88,7 +227,7 @@ public partial class LevelTransition : Area2D {
 
     private void DrawTransitionDirection() {
         var from = Position;
-		var color = Colors.Red;
+        var color = Colors.Red;
 
         var to = from + ExitDirection * DIRECTION_LINE_LENGTH;
         DrawLine(from, to, color, width: 2.0f, antialiased: false);
