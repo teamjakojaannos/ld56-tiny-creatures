fa3024144d2844ff4c0529792960fcf4930b5fd0
Ilari Tommiska <ilari97@hotmail.com>
2024-10-25 04:14:07 +0300

    mostly well-behaved scene transition editor
    It's always pleasantly surprising how much Godot exposes of its
    internals to the scripts. Turns out, one can quite easily query the
    .tscn resource file without instantiating it, in order to determine
    details about the nodes in a given scene. Also turns out things get a
    bit trickier when one wants to perform write operations on the given
    scene.
    
    In order to get modifying the scene entrance via other scene's level
    transition working, we need to be able to do arbitrary edits to a scene
    file, while editing another one. To my surprise and shock, this wasn't
    even as convoluted as I feared.
    
    In practice, the steps to modify the Other Scene are:
     1. load the other scene PackedScene resource with the ResourceLoader
     2. instantiate the other scene (already done for the preview)
     3. make modifications to the instantiated copy (need to undo any
        preview-specific modifications first)
     4. re-pack the instantiated copy into a PackedScene
     5. save the PackedScene resource to a .tscn file via ResourceSaver
     6. overtake the resource path with the modified scene (overwrites the
        cache entry with the modified one)
     7. force editor to reload the scene resource (ensures open editor tabs
        are refreshed)
     8. (re-apply preview-specific modifications to the instantiated copy to
        keep the preview working)
    
    The steps 1 and 2 are more-or less done already, as the preview also has
    to load and instantiate the scene. This creates slight additional
    complexity as e.g. scene root node process mode and position are adjusted
    for the preview, but should remain unchanged when the modifications are
    saved on disk. This requires tracking the original values of those
    properties and restoring them temporarily when the scene is saved.
    
    Most of the steps are clear and there was little to no issues
    implementing those.
    
    The tricky part was getting the resource loader caches and the editor to
    behave nicely with the "external" modifications to the scene files.
    
    The save/load happens eagerly and there is no caching of the resources
    whatsoever , resulting in unnecessary roundtrips to ResourceLoader.
    Also, as the save occurs eagerly immediately as the change occurs, the
    editor freezes for a moment there, making modifying the other scene
    offsets a bit janky.
    
    But it works!

--- levels/LevelTransition.cs
+++ levels/LevelTransition.cs
@@ -18,7 +18,7 @@ public partial class LevelTransition : Area2D {
     private string? _otherScene;
 
     [Export]
-    private NodePath? entranceNodePath;
+    private NodePath entranceNodePath = "";
 
     public bool IsOtherScenePreviewVisible {
         get => _previewScene is not null && _previewScene.IsInsideTree() && !_previewScene.IsQueuedForDeletion();
@@ -29,15 +29,12 @@ public partial class LevelTransition : Area2D {
             return;
         }
 
-        var anchorNode = entranceNodePath is not null
-                                ? _previewScene.GetNodeOrNull<Node2D>(entranceNodePath)
-                                : null;
-        var anchorOffset = anchorNode?.Position ?? Vector2.Zero;
-
-        _previewScene.GlobalPosition = GlobalPosition - anchorOffset;
+        Levels.AdjustLevelPositionRelativeToCurrent(_previewScene, entranceNodePath, this);
     }
 
     private Level? _previewScene;
+    private StringName? previewSceneOriginalName;
+    private Vector2 previewSceneOriginalPosition = Vector2.Zero;
 
     public override string[] _GetConfigurationWarnings() {
         return (base._GetConfigurationWarnings() ?? Array.Empty<string>())
@@ -54,19 +51,36 @@ public partial class LevelTransition : Area2D {
         }
 	}
 
+    private void OpenOtherScene() {
+        if (_otherScene is not null && Engine.IsEditorHint()) {
+            EditorInterface.Singleton.CallDeferred(EditorInterface.MethodName.OpenSceneFromPath, OtherScene);
+        }
+    }
+
 	public override Godot.Collections.Array<Godot.Collections.Dictionary> _GetPropertyList() {
         var properties = base._GetPropertyList() ?? new Godot.Collections.Array<Godot.Collections.Dictionary>();
 
         // If the other scene is specified, obtain a list of valid entrance
         // nodes and show a selection dropdown.
         if (_otherScene is not null) {
+            var editIconName = "Edit";
+            properties.Add(new Godot.Collections.Dictionary() {
+                { "name", "OpenOtherScene" },
+                { "type", (int)Variant.Type.Callable },
+                { "hint", (int)PropertyHint.ToolButton },
+                { "hint_string", $"{MethodName.OpenOtherScene},{editIconName}" },
+                { "usage", (int)(PropertyUsageFlags.Editor | PropertyUsageFlags.NoInstanceState) },
+            });
+
             var entranceNodeNames = FindOtherSceneEntrances()
                 .Select(node => node.Item2);
 
-            var hintString =
-                !entranceNodeNames.Any()
-                    ? "<none available>"
-                    : string.Join(",", entranceNodeNames);
+			bool isEntranceAvailable = entranceNodeNames.Any();
+
+			var hintString =
+                isEntranceAvailable
+					? string.Join(",", entranceNodeNames)
+                    : "<none available>";
             properties.Add(new Godot.Collections.Dictionary() {
                 { "name", "EntranceNode" },
                 { "type", (int)Variant.Type.Int },
@@ -74,6 +88,19 @@ public partial class LevelTransition : Area2D {
                 { "hint_string", hintString },
                 { "usage", (int)(PropertyUsageFlags.Editor | PropertyUsageFlags.NoInstanceState) },
             });
+
+            if (isEntranceAvailable) {
+                // Don't allow modifying the offset without seeing the results
+                var usage =
+                    IsOtherScenePreviewVisible
+                        ? PropertyUsageFlags.Editor | PropertyUsageFlags.NoInstanceState
+                        : PropertyUsageFlags.Editor | PropertyUsageFlags.ReadOnly | PropertyUsageFlags.NoInstanceState;
+                properties.Add(new Godot.Collections.Dictionary() {
+                    { "name", "EntranceNodeOffset" },
+                    { "type", (int)Variant.Type.Vector2 },
+                    { "usage", (int)usage },
+                });
+            }
 
             properties.Add(new Godot.Collections.Dictionary() {
                 { "name", "OtherScenePreview" },
@@ -86,24 +113,40 @@ public partial class LevelTransition : Area2D {
     }
 
     public override Variant _Get(StringName property) {
-        string propertyName = property.ToString();
-        if (propertyName == "EntranceNode") {
-            if (entranceNodePath is not null) {
+        if (property == "EntranceNode") {
+            if (!entranceNodePath.IsEmpty) {
                 var entranceNodes = FindOtherSceneEntrances();
 
                 for (int optionIndex = 0; optionIndex < entranceNodes.Count; optionIndex++) {
-                    var (path, _) = entranceNodes[optionIndex];
+                    var (path, _, _) = entranceNodes[optionIndex];
                     if (path == entranceNodePath) {
                         return optionIndex;
                     }
                 }
             }
 
-            entranceNodePath = null;
+            entranceNodePath = "";
             NotifyPropertyListChanged();
             return 0;
-        } else if (propertyName == "OtherScenePreview") {
+        } else if (property == "OtherScenePreview") {
             return IsOtherScenePreviewVisible;
+        } else if (property == "EntranceNodeOffset") {
+            if (!entranceNodePath.IsEmpty) {
+                var entranceNodes = FindOtherSceneEntrances();
+
+                for (int optionIndex = 0; optionIndex < entranceNodes.Count; optionIndex++) {
+                    var (path, _, position) = entranceNodes[optionIndex];
+                    if (path == entranceNodePath) {
+                        return position;
+                    }
+                }
+            }
+
+            entranceNodePath = "";
+            NotifyPropertyListChanged();
+            return Vector2.Zero;
+        } else if (property == "OpenOtherScene") {
+            return Callable.From(OpenOtherScene);
         }
 
         return default;
@@ -133,39 +176,78 @@ public partial class LevelTransition : Area2D {
             if (valueBool) {
                 var resource = ResourceLoader.Load<PackedScene>(_otherScene);
                 _previewScene = resource.Instantiate<Level>();
+                previewSceneOriginalName = _previewScene.Name;
+                previewSceneOriginalPosition = _previewScene.Position;
                 _previewScene.Name = "Other scene preview";
+                _previewScene.ProcessMode = ProcessModeEnum.Disabled;
 
-                if (entranceNodePath is null) {
+                if (entranceNodePath.IsEmpty) {
                     TrySelectEntranceNode(0);
                 }
 
                 UpdatePreviewPosition();
                 AddChild(_previewScene);
             }
+            NotifyPropertyListChanged();
+        } else if (propertyName == "EntranceNodeOffset" && Engine.IsEditorHint()) {
+            if (_previewScene is not null && _otherScene is not null) {
+                var entranceMarker = _previewScene.GetNode<Node2D>(entranceNodePath);
+                entranceMarker.Position = value.AsVector2();
+
+                var sceneToSave = new PackedScene();
+                _previewScene.Position = previewSceneOriginalPosition;
+                if (previewSceneOriginalName is not null) {
+                    _previewScene.Name = previewSceneOriginalName;
+                }
+                // FIXME: store the original
+                _previewScene.ProcessMode = ProcessModeEnum.Inherit;
+
+                // FIXME: debounce the save a bit
+                sceneToSave.Pack(_previewScene);
+                ResourceSaver.Save(sceneToSave, OtherScene);
+                sceneToSave.TakeOverPath(OtherScene);
+                CallDeferred(MethodName.ReloadPreviewScene);
+
+                // Restore the preview
+                _previewScene.Name = "Other scene preview";
+                _previewScene.ProcessMode = ProcessModeEnum.Disabled;
+
+                if (entranceNodePath.IsEmpty) {
+                    TrySelectEntranceNode(0);
+                }
+
+                UpdatePreviewPosition();
+                NotifyPropertyListChanged();
+            }
         }
         return false;
+    }
+
+    private void ReloadPreviewScene() {
+        EditorInterface.Singleton.ReloadSceneFromPath(OtherScene);
     }
 
     private void TrySelectEntranceNode(int optionIndex) {
         var entranceNodes = FindOtherSceneEntrances();
         if (!entranceNodes.Any()) {
-            entranceNodePath = null;
+            entranceNodePath = "";
             UpdatePreviewPosition();
             return;
         }
 
-        var (path, _) = entranceNodes[Mathf.Min(optionIndex, entranceNodes.Count - 1)];
+        var (path, _, _) = entranceNodes[Mathf.Min(optionIndex, entranceNodes.Count - 1)];
         entranceNodePath = path;
     }
 
-    private List<(NodePath, StringName)> FindOtherSceneEntrances() {
+    private List<(NodePath, StringName, Vector2)> FindOtherSceneEntrances() {
         // Other scene is not properly set yet => return empty list
         if (_otherScene is null || _otherScene.Trim() == "" || _otherScene.Trim() == "res://") {
             return new();
         }
 
-        var entranceNodes = new List<(NodePath, StringName)>();
+        var entranceNodes = new List<(NodePath, StringName, Vector2)>();
 
+        // FIXME: cache the loaded scene to a local field(?)
         var resource = ResourceLoader.Load<PackedScene>(OtherScene);
         var sceneState = resource.GetState();
         var nodesInScene = sceneState.GetNodeCount();
@@ -175,9 +257,20 @@ public partial class LevelTransition : Area2D {
                 continue;
             }
 
+            var position = Vector2.Zero;
+            var nodePropertyCount = sceneState.GetNodePropertyCount(nodeIndex);
+            for (int propertyIndex = 0; propertyIndex < nodePropertyCount; propertyIndex++) {
+                var propertyName = sceneState.GetNodePropertyName(nodeIndex, propertyIndex);
+                if (propertyName == "position") {
+                    position = sceneState.GetNodePropertyValue(nodeIndex, propertyIndex).AsVector2();
+                    break;
+                }
+            }
+
             entranceNodes.Add((
                 sceneState.GetNodePath(nodeIndex),
-                sceneState.GetNodeName(nodeIndex)
+                sceneState.GetNodeName(nodeIndex),
+                position
             ));
         }
 
@@ -185,12 +278,6 @@ public partial class LevelTransition : Area2D {
     }
 
     public Vector2 ExitDirection => Transform.BasisXform(Vector2.Right);
-
-    public override void _ExitTree() {
-        base._ExitTree();
-        _previewScene?.QueueFree();
-        _previewScene = null;
-    }
 
     public override void _Ready() {
         base._Ready();
@@ -213,7 +300,7 @@ public partial class LevelTransition : Area2D {
 
         if (isExiting) {
             GD.Print("Transitioning!");
-            this.Levels().CallDeferred(Levels.MethodName.TransitionToLevel, OtherScene);
+            this.Levels().CallDeferred(Levels.MethodName.TransitionToLevel, OtherScene, entranceNodePath, this);
         }
     }
 
--- levels/Levels.cs
+++ levels/Levels.cs
@@ -28,10 +28,11 @@ public partial class Levels : Node2D {
     }
     private Node2D? _levelParent;
 
-    public void TransitionToLevel(string toScene) {
+    public void TransitionToLevel(string toScene, NodePath entranceNodePath, Node2D exitNode) {
         // Get the instance of the new level. This also forces loading the new
         // scene, in case it hasn't yet been loaded.
         Level level = LoaderFor(toScene).GetLevel();
+        AdjustLevelPositionRelativeToCurrent(level, entranceNodePath, exitNode);
 
         // Move the player to the new scene
         var player = this.Persistent().Player;
@@ -39,6 +40,16 @@ public partial class Levels : Node2D {
 
         // TODO: load adjacent scenes
         // TODO: unload no-longer-adjacent scenes
+        // TODO: re-center the new scene (reposition adjacent scenes relative to new)
+    }
+
+    public static void AdjustLevelPositionRelativeToCurrent(Node2D level, NodePath entranceNodePath, Node2D exitNode) {
+        var anchorNode = entranceNodePath.IsEmpty
+            ? null
+            : level.GetNodeOrNull<Node2D>(entranceNodePath);
+        var anchorOffset = anchorNode?.Position ?? Vector2.Zero;
+
+        level.GlobalPosition = exitNode.GlobalPosition - anchorOffset;
     }
 
     private LevelLoader LoaderFor(string scene) {
