8b62dad63244dd15cc0f59afc9f745ac98d9a8bf
Lassi Kuisma <lassi.kuisma1@gmail.com>
2024-11-08 18:33:36 +0200

    näkki stalk state: chance to dive

--- mobs/nakki_v2/ai_states/NakkiStalkState.cs
+++ mobs/nakki_v2/ai_states/NakkiStalkState.cs
@@ -7,6 +7,7 @@ namespace Jakojaannos.WisperingWoods;
 public partial class NakkiStalkState : NakkiAiState {
 	[Export] private float _stalkTime = 5.0f;
 	[Export] private float _stalkTimeVariation = 0.5f;
+	[Export] private float _diveChance = 0.2f;
 	[Export] private NakkiIdleState? _idleState;
 	[Export] private NakkiAttackState? _attackState;
 	[Export] private NakkiUnderwaterState? _diveState;
@@ -63,8 +64,13 @@ public partial class NakkiStalkState : NakkiAiState {
 
 	public override void DetectionLevelChanged(NakkiV2 nakki) {
 		if (nakki._detectionLevel <= 0.0f) {
-			nakki.SwitchToState(_idleState!);
-			// TODO: roll to dive
+
+			if (_rng.DiceRoll(_diveChance)) {
+				nakki.SwitchToState(_diveState!);
+			} else {
+				nakki.SwitchToState(_idleState!);
+			}
+
 			return;
 		}
 
