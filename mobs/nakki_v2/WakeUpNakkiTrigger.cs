fe19596c9370984808f3fb5b6b08ba05e3f411f2
Lassi Kuisma <lassi.kuisma1@gmail.com>
2024-12-16 16:31:52 +0200

    näkki trigger: [Tool]

--- mobs/nakki_v2/WakeUpNakkiTrigger.cs
+++ mobs/nakki_v2/WakeUpNakkiTrigger.cs
@@ -1,15 +1,28 @@
 using Godot;
+using System;
+using System.Linq;
 
 using Jakojaannos.WisperingWoods.Characters.Player;
 
 namespace Jakojaannos.WisperingWoods;
 
+[Tool]
 public partial class WakeUpNakkiTrigger : Area2D {
+	public override string[] _GetConfigurationWarnings() {
+		var warnings = base._GetConfigurationWarnings() ?? Array.Empty<string>();
+
+		if (_nakkiToTrigger == null) {
+			warnings = warnings.Append("Trigger target is not set").ToArray();
+		}
+
+		return warnings.ToArray();
+	}
+
 	[Export] private NakkiV2? _nakkiToTrigger;
 
 	public override void _Ready() {
-		if (_nakkiToTrigger == null) {
-			GD.PrintErr("Näkki trigger's target is not set!");
+		if (Engine.IsEditorHint()) {
+			return;
 		}
 
 		BodyEntered += OnBodyEntered;
