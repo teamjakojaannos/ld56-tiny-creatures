0ad1373353d957065ce2c448de998f0bb2681c2e
Inari Tommiska <inari@jakojaannos.fi>
2026-05-07 15:40:27 +0300

    clean up unused code, reorg assets

--- Util/Editor/RequireParent.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Jakojaannos.WisperingWoods.Util.Editor;
-
-[AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
-sealed class RequireParentAttribute(params Type[] parentTypes) : Attribute {
-	public Type[] ParentTypes { get; } = parentTypes;
-}
