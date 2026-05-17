cb114a8c5b1854c17c751d716e935e96144a251b
Inari Tommiska <inari@jakojaannos.fi>
2026-05-17 13:55:44 +0300

    fix swamp footstep sounds

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -18,6 +18,7 @@ var is_slowed: bool = false
 var is_invulnerable: bool = false
 var is_dead: bool = false
 var is_in_trouble: bool = false
+var is_wet: bool = false
 var move_speed_modifier: float:
 	get:
 		return 0.5 if is_slowed else 1.0
